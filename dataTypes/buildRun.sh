dc3bbdffb70da1598d89063b268fcf47abcc1a06
leo <chathoo34@gmail.com>
2021-05-25 13:43:34 -0500

    Added a data type reference

---
+++ dataTypes/buildRun.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gcc -odataTypes.o dataTypes.cpp -lstdc++
+
+./dataTypes.o
---
+++ dataTypes/dataTypes.cpp
@@ -0,0 +1,30 @@
+ #include <iostream>
+using namespace std;
+
+//single line comment
+/*Multi Line comment
+ * Haha
+ */
+
+int main() {
+	int myNum = 5;               // Integer (whole number)
+	cout << myNum << "\n";
+	
+	float myFloatNum = 5.99;     // Floating point number
+	cout << myFloatNum << "\n";
+	
+	double myDoubleNum = 9.98;   // Floating point number
+	cout << myDoubleNum << "\n";
+	
+	char myLetter = 'D';         // Character
+	cout << myLetter << "\n";
+	
+	bool myBoolean = true;       // Boolean
+	cout << myBoolean << "\n";
+	
+	string myText = "Hello";     // String 
+	cout << myText << "\n";
+	
+
+	return 0;
+} 
