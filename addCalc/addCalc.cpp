1f3f9f626ba4c77669d48636af182eb02dbcda5f
leo <chathoo34@gmail.com>
2021-05-25 13:35:50 -0500

    Added a simple addition calculator

---
+++ addCalc/addCalc.cpp
@@ -0,0 +1,22 @@
+ #include <iostream>
+using namespace std;
+
+//single line comment
+/*Multi Line comment
+ * Haha
+ */
+
+int main() {
+	int x, y;
+	int sum;
+	
+	cout << "Type a number: ";
+	cin >> x;
+	cout << "Type another number: ";
+	cin >> y;
+	
+	sum = x + y;
+	cout << "Sum is: " << sum << "\n";
+	
+	return 0;
+} 
