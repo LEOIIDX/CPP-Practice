2a4bfaa09da97f057bf2249121e3fc8299349ecf
leo <chathoo34@gmail.com>
2021-05-29 18:48:54 -0500

    started if/else

--- ifelse/ifElse.cpp
+++ ifelse/ifElse.cpp
@@ -5,5 +5,21 @@ using namespace std;
 
 int main() {
 
+	int x = 3;
+
+//	standard if/else
+	if (x == 4) {
+
+		cout << "Oh my god \"x\" equals four!!!!" << "\n";
+
+} else {
+
+		cout << "Damn,\"x\" doesnt equal four" << "\n";
+
+}
+
+//	if else (with else if)
+
+
 	return 0;
 }
