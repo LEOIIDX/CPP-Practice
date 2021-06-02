fc349b74b2f675464b080c099416f2b767e68f39
leo <chathoo34@gmail.com>
2021-06-01 23:00:37 -0400

    before switch statement example

--- ifelse/ifElse.cpp
+++ ifelse/ifElse.cpp
@@ -5,21 +5,34 @@ using namespace std;
 
 int main() {
 
-	int x = 3;
+	int x;
+
+	cout << "Enter in a funny number, haha\n";
+	cin >> x;
 
 //	standard if/else
 	if (x == 4) {
 
 		cout << "Oh my god \"x\" equals four!!!!" << "\n";
 
-} else {
+	} else {
 
 		cout << "Damn,\"x\" doesnt equal four" << "\n";
 
 }
 
 //	if else (with else if)
+    if (x == 3) {
+		cout << "Wowie Zowie, \"x\" equals THREE\n";
+	} else if (x == 5) {
+		cout << "I see, \"x\" equals five\n";
+	} else {
+		cout << "I'm not entirely sure what \"x\" equals\n";
+	}
 
-
+//	Ternary Operator
+	string result = (x < 18) ?
+	"Good day.\n" : "Good evening.\n";
+	cout << result;
 	return 0;
 }
