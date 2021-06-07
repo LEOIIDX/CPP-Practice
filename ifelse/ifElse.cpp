a3c9ba0f20e5627fa1ecc73385df4b72e2981296
leo <chathoo34@gmail.com>
2021-06-07 02:31:57 -0500

    Finished ifElse

--- ifelse/ifElse.cpp
+++ ifelse/ifElse.cpp
@@ -34,5 +34,32 @@ int main() {
 	string result = (x < 18) ?
 	"Good day.\n" : "Good evening.\n";
 	cout << result;
+
+//	Swtich Statement Example
+	switch (x) {
+		case 1:
+			cout << "Monday\n";
+			break;
+		case 2:
+			cout << "Tuesday\n";
+			break;
+		case 3:
+			cout << "Wednesday\n";
+			break;
+		case 4:
+			cout << "Thursday\n";
+			break;
+		case 5:
+			cout << "Friday\n";
+			break;
+		case 6:
+			cout << "Saturday\n";
+			break;
+		case 7:
+			cout << "Sunday\n";
+			break;
+		default:
+			cout << "What?\n";
 	return 0;
+	}
 }
